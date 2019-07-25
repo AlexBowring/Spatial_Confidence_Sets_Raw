1f8e062ae4f5640b42c8dfacd98fb7b043c4e700
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2019-07-25 16:13:10 +0100

    Adding the 2D scripts

---
+++ Sim_03_scripts/Sim_49_480_subjects.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -l h_vmem=4G
+#$ -l h_rt=23:59:00
+#$ -t 1:100
+#$ -cwd
+#$ -o $HOME/log
+#$ -e $HOME/log
+
+. /etc/profile
+
+module add matlab
+
+matlab -nodisplay -nojvm -r Sim_49_480_subjects
+
